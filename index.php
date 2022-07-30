da08e85659edf8c2eabca9e56c9af468f2cb261f
M Maaz Ahmad <95238151+maazi444@users.noreply.github.com>
2022-07-29 17:34:07 -0700

    keplr website v1.0

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/12583d8ffd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Keplr</title>
+</head>
+<body>
+    <main>
+        <section class="main-container">
+            <header>
+                <div class="left-header">
+                    <div class="logoBrand">
+                        <h1>Keplr</h1>
+                    </div>
+                    <ul class="menuLinks">
+                        <li class="active"><a href="#">Payments</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Shop</a></li>
+                    </ul>
+                </div>
+                <div class="right-header">
+                    <a href="#" class="sign-btn">Sign In</a>
+                </div>
+            </header>
+            <div class="main-section">
+               <div class="content-section">
+                <h1 class="main-heading">Focus<br/>On What<br/>Matters</h1>
+                <p class="main-paragraph">Trusted By Over 600,000 Business Worldwide.<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>Vestibulum lacinia arcu eget nulla.</p>
+                <div class="btn-section">
+                    <a href="#" class="main-signin-btn">Sign In</a>
+                    <a href="#" class="main-work-btn">How it works</a>
+                </div>
+               </div>
+            </div>
+        </section>
+        <section class="payment-section">
+            <h1 class="payment-heading">With Keplr It's Easy To Get Paid</h1>
+            <article class="payment-article">
+                <div class="payment-image-section"></div>
+                <div class="payment-content-section">
+                    <div class="payment-content">
+                        <h1 class="content-heading">ELSA FEATURE ONE</h1>
+                        <p class="content-paragraph">Improve Your<br/>Customer<br/>Relations<br/>With Keplr CRM</p>
+                        <p class="content-paragraph-two">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <section class="rightfit-section">
+            <h1 class="payment-heading">Why Keplr is the Right Fit</h1>
+            <article class="rightfit-article">
+                <div class="rightfit-row">
+                    <i class='fas fa-desktop' style='font-size:50px;'></i>
+                    <div class="rightfit-content">
+                        <h1>Manage Accounts</h1>
+                        <p>Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
+                    </div>
+                </div>
+                <div class="rightfit-row">
+                    <i class="fas fa-cart-plus" style='font-size:50px;'></i>
+                    <div class="rightfit-content">
+                        <h1>Simple Checkout</h1>
+                        <p>Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
+                    </div>
+                </div>
+                <div class="rightfit-row">
+                    <i class="fas fa-store-alt" style='font-size:50px;'></i>
+                    <div class="rightfit-content">
+                        <h1>Buyers & Sellers</h1>
+                        <p>Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.</p>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <section class="banner-section">
+            <article class="banner-article">
+                <div class="banner-content">
+                    <h1 class="banner-heading">Accept Every Payment<br/>Quicky, Easily & Securely.</h1>
+                    <a href="#" class="banner-btn">GET STARTED TODAY</a>
+                </div>
+            </article> 
+        </section>
+        <section class="tools-section">
+            <article class="tools-left">
+                <h1 class="tools-heading">Powerful Tools For<br/>Your Business</h1>
+                <div class="tools-outer-row">
+                    <div class="tools-left-row">
+                        <div class="tl-row">
+                            <i class="fas fa-credit-card"></i>
+                            <h1>Easy Cart Features</h1>
+                        </div>
+                        <div class="tl-row">
+                            <i class="fas fa-shopping-cart"></i>
+                            <h1>Lorem ipsum dolor sit amet</h1>
+                        </div>
+                        <div class="tl-row">
+                            <i class="far fa-credit-card"></i>
+                            <h1>Cart Features</h1>
+                        </div>
+                    </div>
+                    <div class="tools-right-row">
+                        <div class="tl-row">
+                            <i class="fas fa-credit-card"></i>
+                            <h1>Easy Cart Features</h1>
+                        </div>
+                        <div class="tl-row">
+                            <i class="fas fa-shopping-cart"></i>
+                            <h1>Lorem ipsum dolor sit amet</h1>
+                        </div>
+                        <div class="tl-row">
+                            <i class="far fa-credit-card"></i>
+                            <h1>Cart Features</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="tools-content">
+                    <h1>Increase the value of<br/>your store with Elsa</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem.</p>
+                </div>
+            </article>
+            <article class="tools-right">
+                <img src="assets/boost-business.png" alt="Powerful Tools for your business">
+            </article>
+        </section>
+        <footer>
+            <article class="footer-row">
+                <h1 class="footer-heading">Keplr</h1>
+                <ul class="footerLinks">
+                    <li><a href="#"><h3>Social</h3></a></li>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fab fa-google"></i></a></li>
+                </ul>
+            </article>
+            <p class="footer-end">Copyright &copy; 2022, Microcomsols</p>
+        </footer>
+    </main>
+</body>
+</html>
